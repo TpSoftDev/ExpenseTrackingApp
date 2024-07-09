08882212cc4d68963bb9a6e8d5c02e7f6c004072
Thabang <tp.softwaredev@gmail.com>
2024-07-08 20:20:20 -0500

    Updated code to summarize expense by category Added a feature to track our daily budget Special character feature was also added for the terminal

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,10 +1,13 @@
 from expense import Expense
+import datetime
+import calendar
 
 
 def main():
     # Welcome message
     print("Welcome To Your Expense Tracker!")
     expense_file_path = "expenses.csv"
+    budget = 2000
 
     # Get user input for an expense
     expense = get_user_expense()
@@ -13,7 +16,7 @@ def main():
     save_user_expense_to_file(expense, expense_file_path)
 
     # Read file and summarize expenses
-    summarize_user_expense(expense_file_path)
+    summarize_user_expense(expense_file_path, budget)
 
 def get_user_expense():
     # Get expense name from user
@@ -64,10 +67,56 @@ def save_user_expense_to_file(expense, expense_file_path):
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.category},{expense.amount}\n")
 
+def summarize_user_expense(expense_file_path, budget):
+    print(f"Summarizing User Expense : {expense_file_path}")
+    expenses = []
+    with open(expense_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            expense_name, expense_category, expense_amount = line.strip().split(",")
+            line_expense = Expense(name=expense_name, category=expense_category, amount=float(expense_amount))
+            expenses.append(line_expense)
+
+    # Group Expenses by category
+    # Data Structure : Dictionary - Look up Item using its key.
+    # Key : Category name (5 Items in dictionary)
+    # Value : Running Total of how much user has spent per category.
+    amount_by_category = {}
+    for expense in expenses:
+        key = expense.category
+        if key in amount_by_category:
+            amount_by_category[key] += expense.amount
+        else:
+            amount_by_category[key] = expense.amount        # Starting Value
+
+    print("Expenses by category")
+    for key, amount in amount_by_category.items():
+        print(f" {key}: ${amount: .2f}")
+
+    total_spent = sum([x.amount for x in expenses])
+    print(f"\nTotal Spent: ${total_spent:.2f}")
+
+    remaining_budget = budget - total_spent
+    print(f"Remaining Budget: ${remaining_budget:.2f}")
+
+    #Daily Budget
+    # Get the current date
+    current_date = datetime.date.today()
+    # Get the last day of the current month
+    _, last_day = calendar.monthrange(current_date.year, current_date.month)
+    # Calculate the remaining days in the current month
+    remaining_days = last_day - current_date.day
+    print(f"The remaining number of days in the current month is: {remaining_days}")
+    daily_budget = remaining_budget / remaining_days
+    print(green(f"Budget Per Day: ${daily_budget:.2f}"))
 
 
-def summarize_user_expense(expense_file_path):
-    pass
+
+#Special Character in terminal
+def green(text):
+    return f"\033[32m{text}\033[0m"
+
+
 
 
 if __name__ == '__main__':
